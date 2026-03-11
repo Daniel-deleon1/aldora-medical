bcd593fe68420ecdf34ea785312d64feab7a962f
Daniel-deleon1 <daniel.deleon.juarez@gmail.com>
2026-03-11 15:12:43 -0600

    Create Home Page component in app/page.js

---
+++ app/page.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <header>
+                <h1>ALDORA MEDICAL</h1>
+                <img src="/path/to/logo.png" alt="ALDORA MEDICAL Logo" />
+            </header>
+            <section>
+                <h2>Featured Products</h2>
+                <ul>
+                    <li>Product 1</li>
+                    <li>Product 2</li>
+                    <li>Product 3</li>
+                </ul>
+            </section>
+        </div>
+    );
+};
+
+export default HomePage;
