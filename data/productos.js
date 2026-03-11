b8a1d6ae74278220f68b01f79e5008a64eb4dd42
Daniel-deleon1 <daniel.deleon.juarez@gmail.com>
2026-03-11 15:12:46 -0600

    Create data/productos.js with surgical products data

---
+++ data/productos.js
@@ -0,0 +1,12 @@
+// Surgical products data
+
+const surgicalProducts = [
+    { name: "Pinza Lane", price: "Q200" },
+    { name: "Speculo Vaginal", price: "Q150" },
+    { name: "Pinza Kelly", price: "Q250" },
+    { name: "Pinza Kocher", price: "Q180" },
+    { name: "Tijera Metzembaum", price: "Q220" },
+    // ... Add more products as needed
+];
+
+module.exports = surgicalProducts;
