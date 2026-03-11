de2a58846f5d7ce11a28e2a9a55e5b74069c93f1
Daniel-deleon1 <daniel.deleon.juarez@gmail.com>
2026-03-11 15:12:33 -0600

    Create next.config.js with production build and optimization settings

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['example.com'], // Add your allowed image domains
+  },
+  productionBrowserSourceMaps: true,
+};
